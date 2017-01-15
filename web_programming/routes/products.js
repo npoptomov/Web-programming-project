1814ea7fc76aa69265dd7bdca19688120555601a
npoptomov <npoptomov@gmail.com>
2017-01-15 21:18:29 +0100

    Update

---
+++ web_programming/routes/products.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var Prod = require('../models/product');
+
+
+router.get('/', function (req, res, next) {
+    res.render('products');
+});
+
+module.exports = router;
